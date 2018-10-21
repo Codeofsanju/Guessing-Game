2c7f33f4efe495faa398d17a40ffb12d8c32865a
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2018-10-21 13:53:28 -0400

    Game Fully Working

--- js/guessing-game.js
+++ js/guessing-game.js
@@ -106,7 +106,6 @@ function play(){
     })
 
     resetButton.addEventListener('click', function(){
-        console.log(game.pastGuesses);
         game = newGame();
         pastGuessArray.innerHTML = 'Take a guess!';
         message.innerHTML = null;

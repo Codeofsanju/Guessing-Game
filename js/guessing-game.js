a365faceab7d30da3ece535d2437e597857caad4
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2018-10-21 22:34:10 -0400

    Header font changed and hint changed to binary

--- js/guessing-game.js
+++ js/guessing-game.js
@@ -2,8 +2,7 @@
 Write your guess-game code here! Don't forget to look at the test specs as a guide. You can run the specs
 by running "testem".
 In this file, you will also include the event listeners that are needed to interact with your HTML file when
-a user clicks a button or adds a guess to the input field.
-*/
+a user clicks a button or adds a guess to the input field.*/
 
 //returns a random number between 1 and 100
 function generateWinningNumber(){
